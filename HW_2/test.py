868a96d9ac7870ea99b3208a773b297b81c7e6d3
Lawrence2005 <lawrenceee0126@gmail.com>
2026-04-16 05:47:26 +0800

    Try

--- HW_2/test.py
+++ HW_2/test.py
@@ -35,7 +35,7 @@ trainset = torchvision.datasets.CIFAR10(
 
 # source and target for testing
 
-for i in range(10):
+for i in range(8):
     for t in[0, 7]:
         source_img, source_class = trainset[i]
         target_class = 7
